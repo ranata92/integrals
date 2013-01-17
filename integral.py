682e62248b217ba9c83f127fb235c2135c910e30
ranata92 <ranata92@gmail.com>
2013-01-17 11:20:54 +0300

    Update integral.py

--- integral.py
+++ integral.py
@@ -1,17 +1,18 @@
-def gates(lower=None,upper=None):
+def gates(*args):
     def decorator(func):
         def wrapped(a,b):
-            if lower and upper:
-                return str(((a/2.0)*upper*upper+b*upper)-((a/2.0)*lower*lower+b*lower))
-            else:
-                return func(a,b)
+            return func(a, b, *args)
         return wrapped
     return decorator
 
+
 @gates(1,2)
-def integral(a,b):
-    a=a/2.0
-    return str(a)+'x^2 + '+ str(b) + 'x+C'
+def integral(a,b,*args):
+    if args:
+        return str(((a/2.0)*args[1]*args[1]+b*args[1])-((a/2.0)*args[0]*args[0]+b*args[0]))
+    else:
+        a=a/2.0
+        return str(a)+'x^2 + '+ str(b) + 'x+C'
 
 def main():
     print integral(2,3)
