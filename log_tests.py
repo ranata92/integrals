9afc2ea9b4509084519ab3efc02fa8d639a7d17a
o_merzlyakov <o_merzlyakov@wargaming.net>
2013-01-24 11:42:32 +0300

    Оформляй кодpython log_tests.py python log_tests.py

--- log_tests.py
+++ log_tests.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 def log(fun):
     def tmp(*args):
         f=open('log_file.txt','a')
@@ -9,12 +12,15 @@ def log(fun):
         return fun(*args)
     return tmp
 
-@log
-def sum(a,b):
-    return a+b
+#@log
+#def sum(a,b):
+#    return a+b
 
 import unittest
+
+
 class tests(unittest.TestCase):
+
     def setUp(self):
         @log
         def func(a,b):
@@ -22,19 +28,23 @@ class tests(unittest.TestCase):
         self.a=2
         self.b=1
         func(self.a,self.b)
+
     def test_read(self):
         self.assertTrue(open('log_file.txt','r'))
+
     def test_time(self):
         f=open('log_file.txt','r')
         lines=f.readlines()
         parts=(lines[-1]).split()
         string=parts[0]+" "+(parts[1])[:-7]
         self.assertEqual(str(datetime.now())[:-7], string)
+
     def test_name(self):
         f=open('log_file.txt','r')
         lines=f.readlines()
         parts=(lines[-1]).split()
         self.assertEqual('func',parts[2])
+
     def test_args(self):
         f=open('log_file.txt','r')
         lines=f.readlines()
@@ -42,8 +52,8 @@ class tests(unittest.TestCase):
         self.assertEqual('(2, 1)',parts[3]+" "+parts[4])
         
         
-def main():
-    print str(sum(1,2))
+#def main():
+#    print str(sum(1,2))
 
 if __name__=='__main__':
     unittest.main()
