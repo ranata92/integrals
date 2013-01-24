068c8c2a7c8e457e0afe9a8983cc13b97e88209a
o_merzlyakov <o_merzlyakov@wargaming.net>
2013-01-24 11:32:09 +0300

    Оформляй код нормально

--- log_tests.py
+++ log_tests.py
@@ -1,5 +1,7 @@
 import unittest
+
 class tests(unittest.TestCase):
+    
     def setUp(self):
         @log
         def func(a,b):
@@ -7,21 +9,27 @@ class tests(unittest.TestCase):
         self.a=2
         self.b=1
         func(self.a,self.b)
+
     def test_read(self):
         self.assertTrue(open('log_file.txt','r'))
+    
     def test_time(self):
         f=open('log_file.txt','r')
         lines=f.readlines()
         parts=(lines[-1]).split()
         string=parts[0]+" "+(parts[1])[:-7]
         self.assertEqual(str(datetime.now())[:-7], string)
+    
     def test_name(self):
         f=open('log_file.txt','r')
         lines=f.readlines()
         parts=(lines[-1]).split()
         self.assertEqual('func',parts[2])
+    
     def test_args(self):
         f=open('log_file.txt','r')
         lines=f.readlines()
         parts=(lines[-1]).split()
         self.assertEqual('(2, 1)',parts[3]+" "+parts[4])
+
+unittest.main()
